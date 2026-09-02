76427608a5881cb0b3d02671b9ab95fe7c54a16c
dinhngoctuyen4125 <tuyenotk4125@gmail.com>
2026-09-02 23:05:27 +0700

    lmao

--- eval_weight_codellama.sh
+++ eval_weight_codellama.sh
@@ -3,9 +3,9 @@
 
 python eval_weight_codellama.py \
     --ocsvm_repo "Hoaikkk/codebert-ocsvm" \
-    --forget_data "./data/codellama/D_forget.json" \
-    --dep_data "./data/codellama/D_test_U_dep.json" \
-    --nondep_data "./data/codellama/D_test_U_nondep.json" \
+    --forget_data "../Data-Collection/codellama/D_forget.json" \
+    --dep_data "../Data-Collection/codellama/D_test_U_dep.json" \
+    --nondep_data "../Data-Collection/codellama/D_test_U_nondep.json" \
     --ood_base_model "tummitum/codebert-deprecated" \
     --num_test_samples 100 \
     --batch_size 16 \
